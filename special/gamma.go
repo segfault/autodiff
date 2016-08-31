b2490a6da5fc864302a86c766f068982004adbd8
Philipp Benner <benner@molgen.mpg.de>
2016-08-31 09:15:39 +0200

    2016/08/31-09:15:37 (Linux gnosiophobie.molgen.mpg.de unknown)

--- special/gamma.go
+++ special/gamma.go
@@ -9,6 +9,10 @@
 //  Copyright Nikhar Agrawal 2013-14
 //  Copyright Christopher Kormanyos 2013-14
 
+//  Use, modification and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 package special
 
 /* -------------------------------------------------------------------------- */
